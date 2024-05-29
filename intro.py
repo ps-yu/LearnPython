baabe726d6ed721199a27256fd27a54d71d0dc61
Pankaj Sharma <113066121+ps-yu@users.noreply.github.com>
2024-05-29 19:26:14 -0400

    Basics of python

---
+++ intro.py
@@ -0,0 +1,65 @@
+# Variables and data types in python
+
+# Print statement in python
+# print("hello world")
+
+# Numbers do not need quotation to print on the screen
+# print(23)
+
+"""
+Creating variable in python
+Standards for creating python variables-:
+Can have upper and lower case letters as well as numbers or (_)
+Should not start with a number
+"""
+
+# name = "python"
+# level = "beginner"
+# print("This is my first " + name + " and I am a " + level)
+
+""" 
+Data types in python
+Integers (int), String (str), Float, Boolean (True, False), None
+# a = None (No variable stored in the variable yet)
+"""
+
+# Python is case-sensitive
+
+# Types of operators in python
+
+"""
+Arithmetic operators (+,-,/,%)
+Relational operators (==, !=, >, <, >=, <=)
+Assignment operators (=, +=, -=, *=, /=, %=, **=)
+Logical operators (not, and, or)
+"""
+
+"""
+Type conversion (Changing the type of variables)
+Conversion -: Handles by the python interpreter
+Casting -: Manual conversion  
+"""
+# integer
+# a = 2
+# float
+# b = 4.35
+# conversion of int to a float type
+# c = a+b
+# print(c)
+
+# converting string type into integer using int
+# a = int("2")
+# b = 2.34
+# print(a+b)
+
+"""
+Getting the user input from the user in python
+the return of a input from the user will always be a string
+To get the input as an integer type casting is used
+"""
+# name = input("Please enter your name")
+# print("Welcome ", name)
+# number = input("Please enter a number: ")
+# print(type(number))
+# number = int(input("Please enter an integer: "))
+# print(type(number))
