0c4a07155352d6c5c5f475e6bf516e3fc40bff46
Pankaj Sharma <113066121+ps-yu@users.noreply.github.com>
2024-05-29 19:46:42 -0400

    Change the structure

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+"""
+Strings and conditionals in python
+"""
+
+# String concatenation
+# str1 = "Python"
+# str2 = " is a easy language to learn!!"
+# str3 = str1 + str2
+# print(str3)
+
+# length of string
+# print(len(str3))
