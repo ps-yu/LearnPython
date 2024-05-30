27b243bc1e4fb1671d9de37d5183b649cbe62b46
Pankaj Sharma <113066121+ps-yu@users.noreply.github.com>
2024-05-29 21:48:21 -0400

    Learn lists and tuples in  python

---
+++ list_tuples.py
@@ -0,0 +1,41 @@
+"""
+Explore lists and tuples in python
+"""
+
+"""
+List is a datatype similar to arrays in Java
+Can store different dat types
+Lists are mutable
+Lists allow duplicate objects
+"""
+# list1 = ["Python", "is", "easy", 1, 2, 3, 4]
+# print(list1)
+# print(list1[3])
+# Mutability in lists
+# list1[0] = 24
+# print(list1)
+# list1[0] = "Python"
+# print(list1)
+# list1.insert(20, 10)
+# print(list1)
+# print(len(list1))
+
+"""
+Tuples are also built-in data types in python that creates immutable sequence of values
+Similar to arrays in Java. The functions operating on the tuples are same as the lists
+"""
+# tup1 = (1, 2, 3, "tuples")
+# print(tup1[1])
+"""to create a tuple with one value (,) is required"""
+# tup1 = (1)
+# print(type(tup1))
+# tup2 = (1,)
+# print(type(tup2))
+# list1 = []
+# cue1 = input("Please enter the first name: ")
+# cue2 = input("Please enter the second name: ")
+# cue3 = input("Please enter the third name: ")
+# list1.append(cue1)
+# list1.append(cue2)
+# list1.append(cue3)
+# print(list1)
