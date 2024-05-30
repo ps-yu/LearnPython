3ab1c939712f2b1eb058dc42e660f91a88f01f48
Pankaj Sharma <113066121+ps-yu@users.noreply.github.com>
2024-05-29 20:45:04 -0400

    Lean conditionals and strings in python

--- conditionals.py
+++ conditionals.py
@@ -3,10 +3,37 @@ Strings and conditionals in python
 """
 
 # String concatenation
-# str1 = "Python"
+# str1 = "python"
 # str2 = " is a easy language to learn!!"
 # str3 = str1 + str2
 # print(str3)
 
 # length of string
 # print(len(str3))
+
+# indexing of strings in python
+# print(str3[2])
+
+
+# Slicing in python is a process accessing parts of the string
+# print(str3[1:4])
+# print(str3[:4]) print from 0-4
+# print(str3[4:]) print from 4-last index
+# python supports -ive indexing as well
+
+
+# String functions
+# print(str3.endswith("learn!!"))
+# returns boolean(T/F)
+# print(str3.capitalize())
+# print(str3.replace("p", "d"))
+# print(str3.find("l"))
+
+
+# Conditional statements in python
+# number1 = int(input("Please input the first number: "))
+# number2 = int(input("Please input the second number: "))
+# if (not number1 % number2):
+#     print(str(number1) + " is divisible by " + str(number2))
+# else:
+#     print(str(number1) + " is not divisible by " + str(number2))
